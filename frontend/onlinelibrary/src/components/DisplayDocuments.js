d17ed551d6591b4541d8941a0daa6c1178d5471a
Contradicted <sameeaslam22@gmail.com>
2021-12-24 14:28:04 +0000

    Test

--- frontend/onlinelibrary/src/components/DisplayDocuments.js
+++ frontend/onlinelibrary/src/components/DisplayDocuments.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export default function displayDocs() {
+
+    const [state, setState] = useState({
+        booktitle: "",
+        author: "",
+        formate: "",
+        Topic: "",
+        state: "",
+        deaths: 0,
+        cases: 0,
+        date: "",
+        PubYear: 1990,
+    });
+
     return (
         <div style={{ marginTop: 10 }}>
             <h3>Add Data</h3>
