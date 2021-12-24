c1bf69f73dec6d6b0f0de0e65a107d1b58bdfecd
Contradicted <sameeaslam22@gmail.com>
2021-12-24 14:24:06 +0000

    Test

--- frontend/onlinelibrary/src/components/DisplayDocuments.js
+++ frontend/onlinelibrary/src/components/DisplayDocuments.js
@@ -2,6 +2,22 @@ import React from 'react';
 
 export default function displayDocs() {
     return (
-        <h1>Display Documents</h1>
+        <div style={{ marginTop: 10 }}>
+            <h3>Add Data</h3>
+            <form onSubmit={OnSubmit} method="Post">
+                <div className="form-group">
+                    <label>Enter State: </label>
+                    <input className="form-control"
+                        type="text" name="state"
+                        value={state.state} />
+                </div>
+                <div className="form-group">
+                    <label>Enter Date: </label>
+                    <input className="form-control"
+                        type="date" name="date"
+                        value={state.date} />
+                </div>
+            </form>
+        </div>
     )
 }
