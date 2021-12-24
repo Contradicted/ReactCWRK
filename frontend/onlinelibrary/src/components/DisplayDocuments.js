ba4aa7a73149aacf138b534c8e257febd67f46a6
Contradicted <sameeaslam22@gmail.com>
2021-12-24 14:15:59 +0000

    Task 2.5

---
+++ frontend/onlinelibrary/src/components/DisplayDocuments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function displayDocs() {
+    return (
+        <div className='navbar navbar-expand-xs'>
+            <h1>Display Documents</h1>
+        </div>
+    )
+}
